e319db4763b8a3abf156e7bea1a32d2a0d4bda8b
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-04-11 16:22:21 +0500

    add cake silce

---
+++ features/cake/cakeSlice.js
@@ -0,0 +1,21 @@
+const createSlice = require('@reduxjs/toolkit').createSlice
+
+const initialState = {
+    numOfCakes: 10
+}
+
+const cakeSlice = createSlice({
+    name: 'cake',
+    initialState,
+    reducers: {
+        orderCake: (state) => {
+            state.numOfCakes--
+        },
+        restockCake: (state, action) => {
+            state.numOfCakes = action.payload
+        }
+    }
+})
+
+module.exports = cakeSlice.reducer
+module.exports.cakeActions = cakeSlice.actions
